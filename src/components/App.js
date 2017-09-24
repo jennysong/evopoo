15277a5f4ed0c9ca36947ba188a28609a04123b2
Shawn Jung <shawn.jung@me.com>
2017-09-24 01:51:33 -0700

    wip

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Header from './Header'
 import Landing from './Landing'
+import "./App.css"
 
 class App extends React.Component {
   render() {
     return (
       <div id="App">
         <Header/>
+        <div id='AppBody'>
+          <Landing/>
+        </div>
       </div>
     );
   }
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Button } from 'semantic-ui-react'
+import logo from "./logo.svg"
+import image from "./Landing-image.jpg"
+import "./Landing.css"
 
-const Landing = () => (
-  <div>
-    Landing
-  </div>
-)
+class Landing extends React.Component {
+  render() {
+    return(
+      <div id='AppLanding'>
+        <img src={logo} className='AppLogo'/>
+        <div className='Description'>
+          Donec sed odio dui. Maecenas sed diam eget risus varius blandit sit amet non magna. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas faucibus mollis interdum. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+        </div>
+        <Button inverted>Get Started</Button>
+        <div className='BackgroundImage'>
+          <img src={image}/>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Landing
