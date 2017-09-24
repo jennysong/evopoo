d22fcb33520ee67fe04ee5936f9a8047b8679b82
Shawn Jung <shawn.jung@me.com>
2017-09-24 09:07:43 -0700

    added map view

--- src/components/DriverApp.js
+++ src/components/DriverApp.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Header'
+import Map from './Map'
 
 class DriverApp extends React.Component {
   render() {
     return (
-      <div>
+      <div id='DriverApp'>
         <Header/>
+        <Map/>
       </div>
     )
   }
