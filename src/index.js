9854d25d02419522a8d5e21fdc89016de1a63ef3
Jenny Song <jenny.jieun.song@gmail.com>
2017-09-24 01:52:29 -0700

    a

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 import reducer from './reducers'
 import './semantic/dist/semantic.min.css';
 import App from './components/App';
