0f81805ea14006a6d81e4784d310f65f63b71a6a
Jenny Song <jenny.jieun.song@gmail.com>
2017-09-24 09:13:49 -0700

    stuff

--- src/components/DriverApp.js
+++ src/components/DriverApp.js
@@ -1,18 +1,51 @@
 import React from 'react';
 import Header from './Header'
 import Map from './Map'
+import { Button, Input } from 'semantic-ui-react'
 
 class DriverApp extends React.Component {
   render() {
     return (
-      <div id='DriverApp'>
+      <div>
         <Header/>
         <Map/>
+        <div>
+          <div id="mappy" style={mappyStyle}></div>
+          
+          <div>From:</div>
+          <Input id="autocompleteFrom" placeholder='Enter address...' />
+          <Button onClick={clickPoint}>Click on map</Button>
+          
+          <div>To:</div>
+          <Input id="autocompleteTo" placeholder="Enter address" />
+          <Button onClick={clickPoint}>Click on map</Button>
+          
+          <div>Time:</div>
+          <Input id="date" type="date" />
+          <Input id="time" type="time" />
+          
+          <div>How many passengers:</div>
+          <Input id="passengers" type="text" />
+
+          <Button onClick={sendData}>Submit</Button>
+        </div>
+
       </div>
     )
   }
 
+}
 
+const mappyStyle = {
+  width: "400px", 
+  height: "500px"
+}
+
+const clickPoint = (type) => {
+  alert('click');
+}
+const sendData = () => {
+  alert('send');
 }
 
 export default DriverApp;
